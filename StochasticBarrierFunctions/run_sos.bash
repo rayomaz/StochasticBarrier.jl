9abcb450e60c084fe3c617eb00ca48dcb19f8c40
Rayan Mazouz <rayan.mazouz@colorado.edu>
2025-11-04 19:34:02 -0700

    Adding comma

--- StochasticBarrierFunctions/run_sos.bash
+++ StochasticBarrierFunctions/run_sos.bash
@@ -44,7 +44,7 @@ yaml_files = [
     "benchmarks/polynomial/systems/oscillator/SOS/sos_deg4.yaml",
     "benchmarks/polynomial/systems/oscillator/SOS/sos_deg6.yaml",
     "benchmarks/polynomial/systems/oscillator/SOS/sos_deg8.yaml",
-    "benchmarks/polynomial/systems/oscillator/SOS/sos_deg12.yaml"
+    "benchmarks/polynomial/systems/oscillator/SOS/sos_deg12.yaml",
 
     "benchmarks/polynomial/systems/room/SOS/sos_deg2.yaml",
     "benchmarks/polynomial/systems/room/SOS/sos_deg4.yaml",
