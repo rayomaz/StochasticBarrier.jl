36296d4672a9257014368e93ce1a89737504ae74
Rayan Mazouz <rayan.mazouz@colorado.edu>
2025-11-11 15:28:45 -0700

    Updating unicycle file

--- StochasticBarrierFunctions/examples/unicycle/unicycle.jl
+++ StochasticBarrierFunctions/examples/unicycle/unicycle.jl
@@ -1,30 +1,31 @@
-using Revise, BenchmarkTools
+# using Revise, BenchmarkTools
 using StochasticBarrierFunctions, LazySets
 using YAXArrays, NetCDF
+using StochasticBarrierFunctions.Data
 
 # System
 system_flag = "unicycle"
-number_hypercubes = 27000
-σ = [0.1, 0.05, 0.01, 0.05]
+number_hypercubes = 1250
+σ = [0.01, 0.01, 0.01, 0.01]
 
-filename = "models/$system_flag/probability_data_$(number_hypercubes)_sigma_$σ.nc"
+filename = "../models/$system_flag/probability_data_$(number_hypercubes)_sigma_$σ.nc"
 dataset = open_dataset(joinpath(@__DIR__, filename))
 
 probabilities = load_probabilities(dataset)
 
-initial_region = Hyperrectangle([-0.20, -0.20, 0.005, 1.5e-1], [0.05, 0.05, 0.005, 0.05])
+initial_region = Hyperrectangle([-0.01, -0.01, -0.01, -0.01], [0.01, 0.01, 0.01, 0.01])
 obstacle_region = EmptySet(4)
 
 # # Optimize: method 1 (revise beta values)
-# @time res_ub = synthesize_barrier(UpperBoundAlgorithm(), probabilities, initial_region, obstacle_region)
+@time res_ub = synthesize_barrier(UpperBoundAlgorithm(), probabilities, initial_region, obstacle_region)
 
-# # Optimize: method 2 (dual approach)
-# @time res_dual = synthesize_barrier(DualAlgorithm(), probabilities, initial_region, obstacle_region)
+# Optimize: method 2 (dual approach)
+@time res_dual = synthesize_barrier(DualAlgorithm(), probabilities, initial_region, obstacle_region)
 
 # # Optimize: method 3 (iterative approach)
-# @time res_it = synthesize_barrier(IterativeUpperBoundAlgorithm(), probabilities, initial_region, obstacle_region)
+@time res_it = synthesize_barrier(IterativeUpperBoundAlgorithm(), probabilities, initial_region, obstacle_region)
 
-# Optimize: method 4 (project gradient descent approach)
+# # Optimize: method 4 (project gradient descent approach)
 @time res_pgd = synthesize_barrier(GradientDescentAlgorithm(), probabilities, initial_region, obstacle_region)
 
 println("Unicycle model verified.")
